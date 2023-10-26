dd7dc056c1fec0da8016c200dd4bebd9fc304893
Cloud <ruben.as532@gmail.com>
2023-10-26 10:04:05 +0200

    Added Circle Figures Checker

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
 
+define('CIRCLE_FIGURES',['0','6','8','9']);
 
+function checkCircleFigure(string $figure): bool {
+	return in_array($figure, CIRCLE_FIGURES);
+}
+
+function getCircleFiguresNumber(string $number) {
+	$number_array = str_split($number);
+	foreach($number_array as $figure) {
+		var_dump(checkCircleFigure($figure));
+	}	
+}
 
 $number = readline("Give me the number to check");
-echo $number.PHP_EOL;
-
+getCircleFiguresNumber($number);
 
 ?>
